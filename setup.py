a650868291c881be19b768145b8aeebfe15d6503
Matt Martz <matt@sivel.net>
2016-02-02 11:43:19 -0600

    Make changes to allow contrib to be installed

--- setup.py
+++ setup.py
@@ -13,43 +13,55 @@ except ImportError:
             " install setuptools).")
     sys.exit(1)
 
-setup(name='ansible',
-      version=__version__,
-      description='Radically simple IT automation',
-      author=__author__,
-      author_email='support@ansible.com',
-      url='http://ansible.com/',
-      license='GPLv3',
-      # Ansible will also make use of a system copy of python-six if installed but use a
-      # Bundled copy if it's not.
-      install_requires=['paramiko', 'jinja2', "PyYAML", 'setuptools', 'pycrypto >= 2.6'],
-      package_dir={ '': 'lib' },
-      packages=find_packages('lib'),
-      package_data={
-         '': ['module_utils/*.ps1', 'modules/core/windows/*.ps1', 'modules/extras/windows/*.ps1', 'galaxy/data/*'],
-      },
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Information Technology',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-          'Natural Language :: English',
-          'Operating System :: POSIX',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Topic :: System :: Installation/Setup',
-          'Topic :: System :: Systems Administration',
-          'Topic :: Utilities',
-      ],
-      scripts=[
-         'bin/ansible',
-         'bin/ansible-playbook',
-         'bin/ansible-pull',
-         'bin/ansible-doc',
-         'bin/ansible-galaxy',
-         'bin/ansible-vault',
-      ],
-      data_files=[],
+setup(
+    name='ansible',
+    version=__version__,
+    description='Radically simple IT automation',
+    author=__author__,
+    author_email='support@ansible.com',
+    url='http://ansible.com/',
+    license='GPLv3',
+    # Ansible will also make use of a system copy of python-six if installed but use a
+    # Bundled copy if it's not.
+    install_requires=['paramiko', 'jinja2', "PyYAML", 'setuptools', 'pycrypto >= 2.6'],
+    package_dir={
+        '': 'lib',
+        'ansible_contrib': 'ansible_contrib'
+    },
+    packages=find_packages('lib') + find_packages(include=('ansible_contrib',)),
+    package_data={
+        '': [
+            'module_utils/*.ps1',
+            'modules/core/windows/*.ps1',
+            'modules/extras/windows/*.ps1',
+            'galaxy/data/*'
+        ],
+        'ansible_contrib': [
+            'README.md',
+            '*/*'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+    scripts=[
+       'bin/ansible',
+       'bin/ansible-playbook',
+       'bin/ansible-pull',
+       'bin/ansible-doc',
+       'bin/ansible-galaxy',
+       'bin/ansible-vault',
+    ],
 )
