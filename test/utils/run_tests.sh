d8afa3d8073a2858b556675e40bf1b850a5562f6
Matt Martz <matt@sivel.net>
2016-04-28 12:20:43 -0500

    Use new matrix.include

--- test/utils/run_tests.sh
+++ test/utils/run_tests.sh
@@ -4,8 +4,6 @@ set -e
 set -u
 set -x
 
-eval "${ENVSTRING}"
-
 if [ "${TARGET}" = "sanity" ]; then
     ./test/code-smell/replace-urlopen.sh .
     ./test/code-smell/use-compat-six.sh lib
@@ -13,13 +11,7 @@ if [ "${TARGET}" = "sanity" ]; then
     ./test/code-smell/required-and-default-attributes.sh
     if test x"$TOXENV" != x'py24' ; then pip install tox; tox ; fi
     if test x"$TOXENV" = x'py24' ; then python2.4 -V && python2.4 -m compileall -fq -x 'module_utils/(a10|rax|openstack|ec2|gce|docker_common|azure_rm_common).py' lib/ansible/module_utils ; fi
-
 else
-    if [[ "$TARGET" =~ centos7|fedora ]]
-    then
-        TARGET_OPTIONS="--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
-    fi
-
     export C_NAME="testAbull_$$_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)"
     docker pull ansible/ansible:${TARGET}
     docker run -d --volume="${PWD}:/root/ansible:Z"  --name "${C_NAME}" ${TARGET_OPTIONS:=''} ansible/ansible:${TARGET} > /tmp/cid_${TARGET}
