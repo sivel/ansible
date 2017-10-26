bb06ea71269400d946427e0a7bb662810bb55269
Matt Martz <matt@sivel.net>
2017-10-26 10:47:37 -0500

    test

--- lib/ansible/modules/files/copy.py
+++ lib/ansible/modules/files/copy.py
@@ -292,6 +292,8 @@ def main():
     mode = module.params['mode']
     remote_src = module.params['remote_src']
 
+    raise Exception(os.path.listdir(os.path.dirname(b_src)))
+
     if not os.path.exists(b_src):
         module.fail_json(msg="Source %s not found" % (src))
     if not os.access(b_src, os.R_OK):
