780d19ce85bb3e791f8150a1b7787dbaa54f4853
Matt Martz <matt@sivel.net>
2020-10-27 12:18:05 -0500

    Fix units

--- test/units/plugins/strategy/test_strategy.py
+++ test/units/plugins/strategy/test_strategy.py
@@ -260,6 +260,7 @@ class TestStrategyBase(unittest.TestCase):
         mock_task.ignore_unreachable = False
         mock_task._uuid = str(uuid.uuid4())
         mock_task.loop = None
+        mock_task.projection = None
         mock_task.copy.return_value = mock_task
 
         mock_handler_task = Handler()
