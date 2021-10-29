45c82df837512c01a77c25224bd72385296af19e
Matt Martz <matt@sivel.net>
2021-10-29 16:32:17 -0500

    Improve accuracy of mock

--- test/units/executor/test_task_executor.py
+++ test/units/executor/test_task_executor.py
@@ -318,7 +318,7 @@ class TestTaskExecutor(unittest.TestCase):
         mock_task.retries = 0
         mock_task.delay = -1
         mock_task.register = {'foo': '.'}
-        mock_task.default_register = 'foo'
+        mock_task.default_register = None
         mock_task.until = None
         mock_task.changed_when = None
         mock_task.failed_when = None
